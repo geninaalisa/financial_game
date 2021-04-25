8b875cb9125ae83b1d48f15ccdd930ec0713ea5c
Genina Alisa <alisa.genina@gmail.com>
2021-04-25 09:48:09 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,138 @@
+from random import randint
+# -*- encoding: utf-8 -*-
+import vk_api
+from random import randint
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+from open_file import Bot
+from datetime import datetime, timedelta
+
+
+token = '1b35f05be43583ccf0fad81740e6fe69409c569515f9ae54680005501ded6cd33637fa2a7a05d957bf157'
+group_id = '200909432'
+session = vk_api.VkApi(token=token)
+vk = session.get_api()
+longpoll = VkBotLongPoll(session, group_id)
+
+game_file = open('game_random.txt', 'r', encoding="utf-8")
+rand_list = []
+for line in game_file.readlines():
+    rand_list.append(line.rstrip())
+
+game_session = {}
+
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+
+        if game_session.get(event.message.from_id) == None:
+            game_session[event.message.from_id] = Bot()
+        game = game_session[event.message.from_id]
+        
+        text = event.message.text
+        
+        if game.money < 0:
+            game.move = 'break'
+        if game.block == '0' and game.income != 0:
+            game.move = 'end'
+
+        if game.move == 'break':
+            message = 'Вы обанкротились.' + 'Конец игры!' + '\n' + '\n' + 'Хотите начать игру заново?'
+            
+            vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id()
+                ) 
+            game_session[event.message.from_id] = Bot()
+
+        if game.move == 'end':
+            message = 'Хотите начать игру заново?'
+            
+            vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id()
+                ) 
+            game_session[event.message.from_id] = Bot()
+
+        if game.move == 'start':
+            message = 'Вы перехали в новый город. С собой у вас 10000 монет.' + '\n' + 'Для начала игры напиши ок.'
+            game.move = 'ask'
+            keyboard3 = VkKeyboard()
+            keyboard3.add_button('Ок', VkKeyboardColor.POSITIVE)
+            vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id(),
+                    keyboard=keyboard3.get_keyboard()
+                )
+
+        elif game.move == 'ask':
+            if text.lower() == 'ок':
+                line = game.open_('game.txt')
+                message = game.text(line)
+                message += '\n' + game.variants(line)
+                game.move = 'choice'
+                vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id(),
+                    keyboard=game.button().get_keyboard()
+                )
+            else:
+                message = 'Я не понимаю.' + '\n' + 'Напиши ок'
+                vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id()
+                )
+                
+        elif game.move == 'choice':
+            choice = text
+            d = game.next_block(line, choice)
+            if d != None:
+                message = 'Деньги: ' + d \
+                + '\n' + 'Доход: ' + str(game.income) \
+                + '\n' + 'Дата: ' + game.date.strftime('%d.%m.%Y') \
+                + '\n' + 'Влияние: ' + str(game.influence) \
+                + '\n' + 'Работа: ' + game.job
+                game.move = 'rand'
+                vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id(),
+                    keyboard=VkKeyboard.get_empty_keyboard()
+                )
+                message = '(Для продолжения нажми ок)'
+                keyboard2 = VkKeyboard()
+                keyboard2.add_button('Ок', VkKeyboardColor.PRIMARY)
+                vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id(),
+                    keyboard=keyboard2.get_keyboard()
+                )
+            else:
+                message = 'Попробуй ещё раз. Я не понимаю.'
+                vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id()
+                )
+
+        
+        if game.move == 'rand':
+            rand = randint(-2, 14)
+            if rand > -1:
+                a = rand_list[rand].split(';')
+                message = a[0]
+                game.money += int(a[1])
+                message += '\n' + 'Деньги: ' + str(game.money)
+            
+                vk.messages.send(
+                    message = message,
+                    peer_id=event.message.from_id,
+                    random_id=get_random_id()
+                ) 
+            game.move = 'ask'
---
+++ open_file.py
@@ -0,0 +1,71 @@
+# -*- encoding: utf-8 -*-
+from random import randint
+import vk_api
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from datetime import datetime, timedelta
+
+class Bot:
+    def __init__(self):
+        self.block = '1'
+        self.money = 10000
+        self.income = 0
+        self.variant = []
+        self.move = 'start'
+        self.date = datetime.today()
+        self.influence = 0
+        self.job = 'У вас её нет'
+        
+
+    def open_(self, file_):
+        game_file = open(file_, 'r', encoding="utf-8")
+        for line in game_file.readlines():
+            line = line.split(';')
+            if line[0] == self.block:
+                return line
+
+    def text(self, line):
+        return line[1]
+    
+    def variants(self, line):
+        st = 'Варианты: '
+        self.variant = []
+        count = 1
+        for i in line:
+            if count != 1 and count != 2:
+                self.variant.append(i)
+            count += 1
+        titles = [v.split('@')[0] for v in self.variant]
+        st += ', '.join(titles)
+        return st
+
+    def next_block(self, line, choice):
+        for i in range(len(self.variant)):
+            x = self.variant[i].split('@')
+            if choice.lower() == x[0].lower():
+                weeeks = randint(2, 5)
+                self.date += timedelta(weeks=weeeks)
+                self.money += self.income
+                self.money += int(x[1])
+                self.income += int(x[2])
+                self.block = x[3].rstrip()
+                self.money += self.income
+                if self.income > 2000:
+                    self.influence += self.income/2000
+                    self.influence = round(self.influence, -3)
+                if self.influence > 100:
+                    self.income += self.influence/100
+                if self.block == '4':
+                    self.job = choice
+                return str(self.money)
+        return None 
+
+    def button(self):
+        keyboard = VkKeyboard()
+        c = 0
+        for i in self.variant:
+            lst = i.split('@')
+            keyboard.add_button(lst[0], VkKeyboardColor.SECONDARY)
+            c += 1
+            if c % 3 == 0 and c < len(self.variant):
+                keyboard.add_line()
+        return keyboard
